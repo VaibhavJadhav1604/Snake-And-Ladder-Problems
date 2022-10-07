b1fe473d4e3ae381dff8520bc9976429dcb22f8f
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-07 20:24:32 +0530

    Update SL7.cs

--- SL7.cs
+++ SL7.cs
@@ -16,17 +16,20 @@ namespace SL7
             Random random = new Random();
             while (position < Won)
             {
+                //Counter Increments Every Time When Condition Is True
                 diecnt++;
                 int ran = random.Next(1, 7);
                 int opt = random.Next(0, 3);
                 switch (opt)
                 {
                     case NoPlay:
+                        //If Random Number Is 0
                         Console.WriteLine("You Got No Play");
                         Console.WriteLine("--------------------------");
                         break;
 
                     case Ladder:
+                        //If Random Number Is 1
                         position = currentpos;
                         if (position < Won)
                         {
@@ -37,6 +40,7 @@ namespace SL7
                                 currentpos = position;
                             }
                         }
+                        //If Ladder Then Player Will Get One More Chance
                         int r = random.Next(1, 7);
                         if (position < Won)
                         {
@@ -52,6 +56,7 @@ namespace SL7
                         break;
 
                     case Snake:
+                        //If Random Number Is 2
                         if (currentpos > 0)
                         {
                             Console.WriteLine("Player Is Bitten By Snake By:" + ran);
@@ -78,11 +83,14 @@ namespace SL7
         static void Main(string[] args)
         {
             SL7 ob1 = new SL7();
+            //For Player 1
             Console.WriteLine("##################################For Player 1##################################");
             int a=ob1.Dice();
-             SL7 ob2=new SL7();
+            SL7 ob2=new SL7();
+            //For Player 2
             Console.WriteLine("##################################For Player 2##################################");
             int b=ob2.Dice();
+            //Comparing Both Who Wins
             if(b<a)
             {
                 Console.WriteLine("Player 2 Has Won");
