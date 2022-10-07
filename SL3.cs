b0fc3b059eeec6a58d8ac1a7a7e122edf7c6eb18
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-07 20:05:56 +0530

    Update SL3.cs

--- SL3.cs
+++ SL3.cs
@@ -11,21 +11,24 @@ namespace SL3
             const int Ladder = 1;
             const int Snake = 2;
                 Random r = new Random();
-                int die = r.Next(1, 7);
-                int opt = r.Next(1, 3);
+                int die = r.Next(1, 7);//Random Number Between 1 to 6 Including 6
+                int opt = r.Next(1, 3);//Random Number Between 1 to 2 Including 2
                 switch (opt)
                 {
                     case NoPlay:
+                        //If Random Number Is 0
                         Console.WriteLine("You're At:" + position);
                         break;
 
                     case Ladder:
+                        //If Random Number Is 1
                         position += die;
                         Console.WriteLine("You're At:" + position);
                         break;
 
                     case Snake:
-                    position -= die;
+                        //If Random Number Is 2
+                        position -= die;
                         if(position < 0)
                         {
                             position = 0;
